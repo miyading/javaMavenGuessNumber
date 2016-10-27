bdcd221c49ab03851dfcd33882022ad547642210
mlding <mlding@thoughtworks.com>
2016-10-27 11:28:06 +0800

    [Mengli]-add all test scenarias

--- src/main/java/CompareNumber.java
+++ src/main/java/CompareNumber.java
@@ -1,14 +1,14 @@
 public class CompareNumber {
   public String getTips(String input, String answer) {
+    
     int aCount = 0;
+    int containCount = 0;
+    
     for (int i = 0; i < input.length(); i++) {
       if (answer.charAt(i) == input.charAt(i)) {
         aCount++;
       }
-    }
-    
-    int containCount = 0;
-    for (int i = 0; i < input.length(); i++) {
+  
       if (answer.contains(String.valueOf(input.charAt(i)))) {
         containCount++;
       }
--- src/test/java/CompareNumberTest.java
+++ src/test/java/CompareNumberTest.java
@@ -15,6 +15,17 @@ public class CompareNumberTest {
   }
   
   @Test
+  public void should_return_3A0B_when_get_tips_given_input_is_1234_and_answer_is_1235() {
+    String input = "1234";
+    String answer = "1235";
+    
+    CompareNumber compareNumber = new CompareNumber();
+    String result = compareNumber.getTips(input, answer);
+    
+    assertThat(result).isEqualTo("3A0B");
+  }
+  
+  @Test
   public void should_return_0A0B_when_get_tips_given_input_is_1234_and_answer_is_5678() {
     String input = "1234";
     String answer = "5678";
@@ -25,6 +36,36 @@ public class CompareNumberTest {
   }
   
   @Test
+  public void should_return_0A1B_when_get_tips_given_input_is_1234_and_answer_is_2678() {
+    String input = "1234";
+    String answer = "2678";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("0A1B");
+  }
+  
+  @Test
+  public void should_return_0A2B_when_get_tips_given_input_is_1234_and_answer_is_2178() {
+    String input = "1234";
+    String answer = "2178";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("0A2B");
+  }
+  
+  @Test
+  public void should_return_0A3B_when_get_tips_given_input_is_1234_and_answer_is_3127() {
+    String input = "1234";
+    String answer = "3127";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("0A3B");
+  }
+  
+  @Test
   public void should_return_0A4B_when_get_tips_given_input_is_1234_and_answer_is_4321() {
     String input = "1234";
     String answer = "4321";
@@ -32,5 +73,75 @@ public class CompareNumberTest {
     String result = new CompareNumber().getTips(input, answer);
     
     assertThat(result).isEqualTo("0A4B");
+  }
+  
+  @Test
+  public void should_return_1A0B_when_get_tips_given_input_is_1234_and_answer_is_1789() {
+    String input = "1234";
+    String answer = "1789";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("1A0B");
+  }
+  
+  @Test
+  public void should_return_1A1B_when_get_tips_given_input_is_1567_and_answer_is_1872() {
+    String input = "1567";
+    String answer = "1872";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("1A1B");
+  }
+  
+  @Test
+  public void should_return_1A2B_when_get_tips_given_input_is_1234_and_answer_is_1328() {
+    String input = "1234";
+    String answer = "1328";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("1A2B");
+  }
+  
+  @Test
+  public void should_return_1A3B_when_get_tips_given_input_is_1234_and_answer_is_1423() {
+    String input = "1234";
+    String answer = "1423";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("1A3B");
+  }
+  
+  @Test
+  public void should_return_2A0B_when_get_tips_given_input_is_1234_and_answer_is_1289() {
+    String input = "1234";
+    String answer = "1289";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("2A0B");
+  }
+  
+  @Test
+  public void should_return_2A1B_when_get_tips_given_input_is_1234_and_answer_is_1249() {
+    String input = "1234";
+    String answer = "1249";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("2A1B");
+  }
+  
+  @Test
+  public void should_return_2A2B_when_get_tips_given_input_is_1234_and_answer_is_1243() {
+    String input = "1234";
+    String answer = "1243";
+    
+    String result = new CompareNumber().getTips(input, answer);
+    
+    assertThat(result).isEqualTo("2A2B");
   }
 }
